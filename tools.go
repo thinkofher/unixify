265a944befc3455e1fd330598bcfcc004bdaf5d3
thinkofher <piaskupiasku@gmail.com>
2019-08-20 13:37:00 +0200

    Created askUser function for asking for permission

---
+++ tools.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func askUser(question string) (bool, error) {
+	fmt.Printf("%s: %s (y/N): ", appName, question)
+	reader := bufio.NewReader(os.Stdin)
+
+	char, _, err := reader.ReadRune()
+	if err != nil {
+		return false, err
+	}
+
+	if unicode.ToLower(char) == 'y' {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
