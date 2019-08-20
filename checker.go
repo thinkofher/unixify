30d5af5badf5ae8636c82ba526350ccd2f6249c1
thinkofher <piaskupiasku@gmail.com>
2019-08-20 22:47:32 +0200

    Created documentation in checker.go file

--- checker.go
+++ checker.go
@@ -6,8 +6,12 @@ import (
 	"strings"
 )
 
+// checker represents function for auditing
+// os.FileInfo interfaces.
 type checker func(os.FileInfo) bool
 
+// applyCheckers returns slice of os.FileInfo containing
+// only os.FileInfo interfaces, fulfilling given checkers.
 func applyCheckers(files []os.FileInfo, checkers ...checker) []os.FileInfo {
 	var ans []os.FileInfo
 
@@ -22,28 +26,42 @@ func applyCheckers(files []os.FileInfo, checkers ...checker) []os.FileInfo {
 	return ans
 }
 
+// isDir returns true if given file is directory.
 func isDir(file os.FileInfo) bool { return file.IsDir() }
 
+// isHidden returns true if given file is hidden.
 func isHidden(file os.FileInfo) bool {
 	return strings.HasPrefix(file.Name(), ".")
 }
 
+// isHiddenNonDir returns true if given file is hidden
+// and is not directory.
 func isHiddenNonDir(file os.FileInfo) bool {
 	return !isDir(file) && isHidden(file)
 }
 
+// isDirNonHidden returns true if given file is directory
+// and is not hidden.
 func isDirNonHidden(file os.FileInfo) bool {
 	return !isHidden(file) && isDir(file)
 }
 
+// isHiddenOrDir returns true if given file is directory
+// or is hidden.
 func isHiddenOrDir(file os.FileInfo) bool {
 	return isDir(file) || isHidden(file)
 }
 
+// isNormal return true if given file is not directory
+// and is not hidden.
 func isNormal(file os.FileInfo) bool {
 	return !isDir(file) && !isHidden(file)
 }
 
+// makeOrders returns pointer to slice of orders created from
+// given root filepath and slice of os.FileInfos. Function uses
+// filepath.Join function to merge filenames and given root filepath,
+// which makes makeOrders portable.
 func makeOrders(root string, files []os.FileInfo) *[]order {
 	var ans []order
 	for _, file := range files {
