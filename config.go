86979319fb684b3da99766fd12394120fe886093
thinkofher <piaskupiasku@gmail.com>
2019-08-21 09:12:51 +0200

    Created raw version of usage

--- config.go
+++ config.go
@@ -4,3 +4,37 @@ var appName = "unixify"
 var version = "0.1.0"
 var author = "Beniamin Dudek"
 var email = "beniamin.dudek@yahoo.com"
+
+var usageRaw = `USAGE:
+    {{appName}} [OPTIONS]
+
+INFO:
+    Simple portable command line interface tool
+    for changing file and folder names to be more
+    unixy.
+
+    Example:
+
+    * "Some File"         -> "some_file"
+    * "sOme File"         -> "some_file"
+    * "SOMEFILE"          -> "some_file"
+    * ".some hidden file" -> ".some_hidden_file"
+
+AUTHOR:
+    {{author}} <{{email}}>
+
+VERSION:
+    {{version}}
+
+OPTIONS:
+    -dirs
+        include directories
+    -hidden
+        include hidden files
+    -interactive
+        asks before every rename
+    -verbose
+        print current actions
+    -yes
+        skip asking to rename at startup
+`
